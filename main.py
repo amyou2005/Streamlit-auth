e3b0f9a0cfa5df8b909047bc87e5844422c0f3eb
THIRAYU MEERERKSOM <amyou.sc@gmail.com>
2023-10-03 20:11:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title='Streamlit', page_icon='🐍', initial_sidebar_sta
     #     usernames.append(user['username'])
     #     passwords.append(user['password'])
 
-    # credentials = {'usernames': {}}
+    credentials = {'usernames': {}}
     # for index in range(len(emails)):
     #     credentials['usernames'][usernames[index]] = {'name': emails[index], 'password': passwords[index]}
 

e5b521d5d9835647d5f456e839d82283cd9fab11
THIRAYU MEERERKSOM <amyou.sc@gmail.com>
2023-10-03 20:09:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,40 +21,40 @@ st.set_page_config(page_title='Streamlit', page_icon='🐍', initial_sidebar_sta
     # for index in range(len(emails)):
     #     credentials['usernames'][usernames[index]] = {'name': emails[index], 'password': passwords[index]}
 
-    Authenticator = stauth.Authenticate(credentials, cookie_name='Streamlit', key='kasira', cookie_expiry_days=4)
+Authenticator = stauth.Authenticate(credentials, cookie_name='Streamlit', key='kasira', cookie_expiry_days=4)
 
-    email, authentication_status, username = Authenticator.login(':green[Login]', 'main')
+email, authentication_status, username = Authenticator.login(':green[Login]', 'main')
 
-    info, info1 = st.columns(2)
+info, info1 = st.columns(2)
 
     # if not authentication_status:
-    sign_up()
+# sign_up()
 
     # if username:
     #     if username in usernames:
     #         if authentication_status:
                 # let User see app
-    st.sidebar.subheader(f'Welcome {username}')
-    Authenticator.logout('Log Out', 'sidebar')
+st.sidebar.subheader(f'Welcome {username}')
+Authenticator.logout('Log Out', 'sidebar')
 
-    st.subheader('This is the home page')
-    st.markdown(
-        """
-        ---
-        Created by https://kasira.biz
-        
-        """
-    )
+st.subheader('This is the home page')
+st.markdown(
+    """
+    ---
+    Created by https://kasira.biz
+    
+    """
+)
 
             # elif not authentication_status:
             #     with info:
-    st.error('Incorrect Password or username')
+st.error('Incorrect Password or username')
             # else:
             #     with info:
-    st.warning('Please feed in your credentials')
+st.warning('Please feed in your credentials')
         # else:
         #     with info:
-    st.warning('Username does not exist, Please Sign up')
+st.warning('Username does not exist, Please Sign up')
 
 
 # except:
